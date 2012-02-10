fcb7b094bee388383837c09e1e2ff813d733229b
Bas Vodde <basv@odd-e.com>
2012-02-10 09:11:51 +0800

    Almost ready to push out the gem

--- lib/osaka.rb
+++ lib/osaka.rb
@@ -1,7 +1,7 @@
 
-require 'osaka/scriptrunner.rb'
-require 'osaka/applicationwrapper.rb'
-require 'osaka/typicalapplication.rb'
-require 'osaka/pages.rb'
-require 'osaka/numbers.rb'
-require 'osaka/keynote.rb'
+require 'osaka/scriptrunner'
+require 'osaka/applicationwrapper'
+require 'osaka/typicalapplication'
+require 'osaka/pages'
+require 'osaka/numbers'
+require 'osaka/keynote'
---
+++ lib/osaka/version.rb
@@ -0,0 +1,4 @@
+
+module Osaka
+  VERSION = "0.1.0"
+end

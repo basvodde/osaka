a0549da91398c64ffec0d46736f260075442b45f
James W Grenning <james@grenning.net>
2013-06-22 11:42:37 -0500

    removed comment out code

--- spec/keynote_spec.rb
+++ spec/keynote_spec.rb
@@ -20,23 +20,5 @@ describe "Osaka::Keynote" do
     subject.should_receive(:select_all)
     subject.select_all_slides
   end
-
-  # it "Should be possible to select all the slides in the default location" do
-  #   slides_button_location = at.button("Slides").group(1).outline(1).scroll_area(2).splitter_group(1).splitter_group(1)    
-  #   expect_exists?(slides_button_location).and_return(true)
-  #   expect_click(slides_button_location)
-  #   subject.should_receive(:select_all)
-  #   subject.select_all_slides
-  # end
-  # 
-  # it "Should be possible to select all the slides in the alternative location" do
-  #   slides_button_location = at.button("Slides").group(1).outline(1).scroll_area(2).splitter_group(1).splitter_group(1)    
-  #   alternative_slides_button_location = at.button("Slides").group(1).outline(1).scroll_area(1).splitter_group(1).splitter_group(1)
-  #   expect_exists?(slides_button_location).and_return(false)
-  #   expect_click(alternative_slides_button_location)
-  #   subject.should_receive(:select_all)
-  #   subject.select_all_slides
-  # end
-  
   
 end

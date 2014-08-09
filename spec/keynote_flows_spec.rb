f523ce092a8f84470a2ce511c296344c213fc037
James Grenning <james.grenning.net>
2014-08-09 11:00:07 -0500

    Removed deprecated wathings for .should usage

--- spec/keynote_flows_spec.rb
+++ spec/keynote_flows_spec.rb
@@ -4,17 +4,17 @@ require "osaka"
 describe "Common flows in keynote" do
   
   def should_shutdown
-    mock_keynote.should_receive(:save)
-    mock_keynote.should_receive(:close)
-    mock_keynote.should_receive(:quit)
+    expect(mock_keynote).to receive(:save)
+    expect(mock_keynote).to receive(:close)
+    expect(mock_keynote).to receive(:quit)
   end
 
   let(:mock_keynote) { double("First keynote")}
 
   it "Should exit if keynote windows are already open" do
-    Osaka::Keynote.should_receive(:new).and_return(mock_keynote)
-    mock_keynote.should_receive(:activate)
-    mock_keynote.should_receive(:raise_error_on_open_standard_windows)
+    expect(Osaka::Keynote).to receive(:new).and_return(mock_keynote)
+    expect(mock_keynote).to receive(:activate)
+    expect(mock_keynote).to receive(:raise_error_on_open_standard_windows)
       .with("All Keynote windows must be closed before running this flow")
       .and_raise(Osaka::ApplicationWindowsMustBeClosed, "All Keynote windows must be closed before running this flow")
     
@@ -24,12 +24,12 @@ describe "Common flows in keynote" do
   end
   
   it "Should be able to combine just one single file" do
-    Osaka::Keynote.should_receive(:new).and_return(mock_keynote)
-    mock_keynote.should_receive(:activate)
-    mock_keynote.should_receive(:raise_error_on_open_standard_windows)
-    mock_keynote.should_receive(:light_table_view)
-    mock_keynote.should_receive(:open).with("one_file.key")
-    mock_keynote.should_receive(:save_as).with("result.key")
+    expect(Osaka::Keynote).to receive(:new).and_return(mock_keynote)
+    expect(mock_keynote).to receive(:activate)
+    expect(mock_keynote).to receive(:raise_error_on_open_standard_windows)
+    expect(mock_keynote).to receive(:light_table_view)
+    expect(mock_keynote).to receive(:open).with("one_file.key")
+    expect(mock_keynote).to receive(:save_as).with("result.key")
     should_shutdown
     CommonFlows.keynote_combine_files("result.key", "one_file.key")
   end
@@ -37,24 +37,24 @@ describe "Common flows in keynote" do
   it "Should be able to combine multiple files in one result" do
     mock2_keynote = double("Second keynote")
     mock3_keynote = double("Third keynote")
-    Osaka::Keynote.should_receive(:new).and_return(mock_keynote, mock2_keynote, mock3_keynote)  
-    mock_keynote.should_receive(:activate)
-    mock_keynote.should_receive(:raise_error_on_open_standard_windows)
-    mock_keynote.should_receive(:open).with("one_file.key")
-    mock_keynote.should_receive(:light_table_view)
-    mock_keynote.should_receive(:save_as).with("result.key")
-    mock_keynote.should_receive(:select_all_slides).exactly(2).times
-    mock_keynote.should_receive(:paste).exactly(2).times
+    expect(Osaka::Keynote).to receive(:new).and_return(mock_keynote, mock2_keynote, mock3_keynote)  
+    expect(mock_keynote).to receive(:activate)
+    expect(mock_keynote).to receive(:raise_error_on_open_standard_windows)
+    expect(mock_keynote).to receive(:open).with("one_file.key")
+    expect(mock_keynote).to receive(:light_table_view)
+    expect(mock_keynote).to receive(:save_as).with("result.key")
+    expect(mock_keynote).to receive(:select_all_slides).exactly(2).times
+    expect(mock_keynote).to receive(:paste).exactly(2).times
     
-    mock2_keynote.should_receive(:open).with("two_file.key")
-    mock2_keynote.should_receive(:select_all_slides)
-    mock2_keynote.should_receive(:copy)
-    mock2_keynote.should_receive(:close)
+    expect(mock2_keynote).to receive(:open).with("two_file.key")
+    expect(mock2_keynote).to receive(:select_all_slides)
+    expect(mock2_keynote).to receive(:copy)
+    expect(mock2_keynote).to receive(:close)
     
-    mock3_keynote.should_receive(:open).with("three_file.key")
-    mock3_keynote.should_receive(:select_all_slides)
-    mock3_keynote.should_receive(:copy)
-    mock3_keynote.should_receive(:close)
+    expect(mock3_keynote).to receive(:open).with("three_file.key")
+    expect(mock3_keynote).to receive(:select_all_slides)
+    expect(mock3_keynote).to receive(:copy)
+    expect(mock3_keynote).to receive(:close)
     
     should_shutdown
     CommonFlows.keynote_combine_files("result.key", ["one_file.key", "two_file.key", "three_file.key"])    
@@ -63,8 +63,8 @@ describe "Common flows in keynote" do
   it "Should be able to combine multiple files from one directory sorted" do
     files_in_dir = [".", "..", "one.key", "05_file.key", "02key.wrong", "another", "01hey.key", "last"]
     files_matching = ["./05_file.key", "./01hey.key", "./one.key"]
-    Dir.should_receive(:new).with(".").and_return(files_in_dir)
-    CommonFlows.should_receive(:keynote_combine_files).with("results.key", files_matching.sort)
+    expect(Dir).to receive(:new).with(".").and_return(files_in_dir)
+    expect(CommonFlows).to receive(:keynote_combine_files).with("results.key", files_matching.sort)
     CommonFlows.keynote_combine_files_from_directory_sorted("results.key")    
   end
 
@@ -72,9 +72,9 @@ describe "Common flows in keynote" do
     files_in_dir = [".", "..", "05_file.key", "02key.wrong", "another", "01hey.key", "last"]
     files_in_dir_to_be_used = ["dirname/01hey.key", "dirname/05_file.key"]
     mocked_dir = double("Directory with keynote files")
-    Dir.should_receive(:new).with("dirname").and_return(mocked_dir)
-    mocked_dir.should_receive(:entries).and_return(files_in_dir)
-    CommonFlows.should_receive(:keynote_combine_files).with("results.key", files_in_dir_to_be_used)
+    expect(Dir).to receive(:new).with("dirname").and_return(mocked_dir)
+    expect(mocked_dir).to receive(:entries).and_return(files_in_dir)
+    expect(CommonFlows).to receive(:keynote_combine_files).with("results.key", files_in_dir_to_be_used)
     CommonFlows.keynote_combine_files_from_directory_sorted("results.key", "dirname", /^\d+.*\.key$/)    
   end
   

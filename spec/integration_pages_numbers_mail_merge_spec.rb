11c36759c39c0e8c21ee2af6748e0059be729c59
Bas Vodde <basv@odd-e.com>
2015-01-07 16:06:57 +0800

    Removed one integration spec

--- spec/integration_pages_numbers_mail_merge_spec.rb
+++ spec/integration_pages_numbers_mail_merge_spec.rb
@@ -10,11 +10,11 @@ describe "Integration of mail merge flow with Pages and Numbers", :integration =
     @pages_template_filename = File.join(@tempdir, "mail_merge_template.pages")
     @pdf_output_file = File.join(@tempdir, "output.pdf")
   end
-  
+
   after (:each) do
     FileUtils.remove_entry_secure @tempdir
   end
-  
+
   it "Should mail merge the assets and generate a PDF" do
 
     Osaka::Numbers.create_document(@numbers_filename) { |doc|
@@ -23,16 +23,16 @@ describe "Integration of mail merge flow with Pages and Numbers", :integration =
       doc.fill_cell(2, 3, "of Data")
       doc.fill_cell(2, 4, "in Numbers")
     }
-    
+
     Osaka::Pages.create_document(@pages_template_filename) { |doc|
       doc.set_mail_merge_document(@numbers_filename)
-      doc.type("Hello World! This is pages. We're going to mail merge!\r\r")    
+      doc.type("Hello World! This is pages. We're going to mail merge!\r\r")
       doc.mail_merge_field("Data");
     }
-        
+
     CommonFlows::number_and_pages_mail_merge(@numbers_filename, @pages_template_filename, @pdf_output_file)
     expect(File.exists?(@pdf_output_file)).to eq true
-    
+
     preview = Osaka::Preview.new
     preview.open(@pdf_output_file)
     expect(preview.pdf_content).to include("Hello World")
--- spec/integration_preview_spec.rb
+++ spec/integration_preview_spec.rb
@@ -1,16 +1,16 @@
 require 'osaka'
 
-describe "Integration tests for Preview" do
-  
+describe "Integration tests for Preview", :integration => true do
+
   before(:each) do
     @assets_directory =  File.join(File.dirname(__FILE__), "assets")
   end
-        
+
   it "Should be able to open a file and check if there is a certain text" do
     preview = Osaka::Preview.new
     preview.open(File.join(@assets_directory, "document.pdf"))
     expect(preview.pdf_content).to include("Jamaica")
     preview.close
-    preview.quit    
+    preview.quit
   end
 end

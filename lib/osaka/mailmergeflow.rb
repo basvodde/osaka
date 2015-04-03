60d78885d9a8cefee566fd2312e767558e1fcc38
Bas Vodde <basv@odd-e.com>
2015-04-03 13:38:27 +0800

    Fixed white space

--- lib/osaka/mailmergeflow.rb
+++ lib/osaka/mailmergeflow.rb
@@ -1,12 +1,12 @@
 
 module CommonFlows
-  
+
   def self.number_and_pages_mail_merge(numbers_file, pages_file, output_file)
-    
+
     if block_given?
       Osaka::Numbers.create_document(numbers_file) { |numbers|
-        yield numbers         
-      }      
+        yield numbers
+      }
     end
 
     pages = Osaka::Pages.new

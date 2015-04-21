8e1768df8d75749e34fd29dad881fa266405bbca
Bas Vodde <basv@odd-e.com>
2015-04-21 14:04:36 +0800

    Fixed Whitespace

--- spec/integration_numbers_spec.rb
+++ spec/integration_numbers_spec.rb
@@ -2,16 +2,16 @@ require 'tmpdir'
 require 'osaka'
 
 describe "Integration tests for Numbers" do
-  
+
   it "Should be able to fill in large index cells", :integration => true do
-    
+
     Dir.mktmpdir { |dir|
-      
+
       Osaka::Numbers.create_document(File.join(dir, "temp.numbers")) { |doc|
         doc.fill_cell(12, 1, "Data")
       }
     }
-    
+
   end
-  
+
 end

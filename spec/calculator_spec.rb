13a41c76a845fd2dcf57f1237787efa7c7a9f2ab
Bas Vodde <basv@odd-e.com>
2017-01-04 21:18:48 +0800

    Fixed Whitespace

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -7,9 +7,9 @@ describe "Mac GUI Calculator" do
 
   subject { Osaka::Calculator.new }
   let(:control) { subject.control = double("RemoteControl", :mac_version => :mountain_lion)}
-    
+
   it "Should be setting the window when starting the Calculator" do
-        
+
     expect_activate
     expect_current_window_name.and_return("")
     expect(subject).to receive(:wait_for_new_window).with([])
@@ -17,27 +17,27 @@ describe "Mac GUI Calculator" do
     expect_set_current_window("Calculator")
     subject.activate
   end
-    
+
   it "Should be able to click a button on the calculator" do
     expect_click!(at.button("1").group(2))
     subject.click("1")
   end
-  
+
   it "Should be able to use keystroke on the calculator" do
     expect_keystroke("1")
     subject.key("1")
   end
-  
+
   it "Should be able to quit the calculator" do
     expect_running?.and_return(true)
     expect_quit
     subject.quit
   end
-  
+
   it "Should be able to get the value from the screen" do
     expect_wait_until_exists!(at.static_text(1).group(1))
     expect_get!("value", at.static_text(1).group(1)).and_return("0")
     expect(subject.result).to eq "0"
   end
-  
+
 end
